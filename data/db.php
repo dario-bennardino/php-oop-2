eba5d8b7e46e5c06cf1dcf6d9e2b782465d507e6
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-05-05 19:17:06 +0200

    [add] aggiungere classe icon

--- data/db.php
+++ data/db.php
@@ -3,7 +3,6 @@
 require_once __DIR__ . '/../Model/Product.php';
 require_once __DIR__ . '/../FoodProduct.php';
 require_once __DIR__ . '/../BedProduct.php';
-require_once __DIR__ . '/../FoodProduct.php';
 require_once __DIR__ . '/../ToyProduct.php';
 
 
@@ -17,13 +16,13 @@ $db = [
     new BedProduct('Letto Grotta', 'https://m.media-amazon.com/images/I/61wRoGk9I8L._AC_SL1500_.jpg', 'Galate', 32.24, ['gatti'], 'Casetta per gatti di design 2 in 1: le case per gatti sono disponibili in due forme. Lo stile igloo consente al tuo gatto di nascondersi e sentirsi al sicuro', 'medium'),
     new ToyProduct('Palla con luce LED', 'https://m.media-amazon.com/images/I/71vvAwH93+L._AC_SL1500_.jpg', 'Iokheira', 12.99, ['gatti'], 'Gioco per gatti, palla autoruotante 360, elettrica, a due colori, gioco innovativo, ricaricabile', 'silicone'),
     new ToyProduct('Corda da gioco', 'https://m.media-amazon.com/images/I/71MxWPeyBaL._AC_SL1500_.jpg', 'Trixie', 3.49, ['cani'], 'Giocattolo corda da gioco in puro cotone', 'cotone'),
-    new ToyProduct('Palla con luce LED', 'https://m.media-amazon.com/images/I/71MxWPeyBaL._AC_SL1500_.jpg', 'Iokheira', 12.99, ['gatti'], 'Gioco per gatti, palla autoruotante 360, elettrica, a due colori, gioco innovativo, ricaricabile', 'silicone'),
+    new ToyProduct('Palla con luce LED', 'https://m.media-amazon.com/images/I/71vvAwH93+L._AC_SL1500_.jpg', 'Iokheira', 12.99, ['gatti'], 'Gioco per gatti, palla autoruotante 360, elettrica, a due colori, gioco innovativo, ricaricabile', 'silicone'),
 
 
 ];
 
 // Variabile per accedere al prodotto creato
-$product = $db[0];
+// $product = $db[0];
 
 // Stampa le informazioni del prodotto utilizzando var_dump
 // var_dump($product->getFullInfo());
--- index.php
+++ index.php
@@ -14,12 +14,16 @@ require_once __DIR__ . '/data/db.php';
     <title>php-oop-2</title>
     <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- CSS -->
     <link rel="stylesheet" href="./CSS/style.css">
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body>
 
+    <!-- HEADER -->
     <header>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
@@ -40,12 +44,9 @@ require_once __DIR__ . '/data/db.php';
                                 Categories
                             </a>
                             <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <li><a class="dropdown-item" href="#">Cani</a></li>
+                                <li><a class="dropdown-item" href="#">Gatti</a></li>
+
                             </ul>
                         </li>
                         <li class="nav-item">
@@ -61,30 +62,37 @@ require_once __DIR__ . '/data/db.php';
         </nav>
     </header>
 
-    <div class="container">
-        <!-- <h1 class="text-center my-4">Shop Online</h1> -->
-        <div class="row row-cols-4 ">
-            <?php foreach ($db as $product) : ?>
-                <div class="card" style="width: 18rem;">
-                    <img src="<?php echo $product->image ?>" class="card-img-top" alt="<?php echo $product->name; ?>">
-                    <div class="card-body">
-                        <h5 class="card-title"> <?php echo $product->name; ?></h5>
-                        <p class="card-text"> <?php echo $product->description; ?></p>
+    <!-- MAIN -->
+    <main>
+        <div class="container">
+            <!-- <h1 class="text-center my-4">Shop Online</h1> -->
+            <div class="row row-cols-4 ">
+                <?php foreach ($db as $product) : ?>
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?php echo $product->image ?>" class="card-img-top" alt="<?php echo $product->name; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"> <?php echo $product->name; ?></h5>
+                            <p class="card-text"> <?php echo $product->description; ?></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"> <?php echo $product->brand; ?> </li>
+                            <li class="list-group-item">€ <?php echo $product->price; ?></li>
+                            <li class="list-group-item">Adatto per: <?php echo $product->getFullSpecies(); ?></li>
+
+
+                            <li class="list-group-item">Ingredienti: </li>
+
+
+                            <li class="list-group-item">Dimensione: <?php echo $product->size ?? '-'; ?></li>
+                            <li class="list-group-item">Materiale: <?php echo $product->materials ?? '-'; ?></li>
+                        </ul>
                     </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"> <?php echo $product->brand; ?> </li>
-                        <li class="list-group-item">€ <?php echo $product->price; ?></li>
-                        <li class="list-group-item">Adatto per: <?php echo $product->getFullSpecies(); ?></li>
-                        <li class="list-group-item">Ingredienti: ?></li>
-                        <li class="list-group-item">Dimensione: <?php echo $product->size ?? '-'; ?></li>
-                        <li class="list-group-item">Materiale: <?php echo $product->materials ?? '-'; ?></li>
-                    </ul>
-                </div>
-            <?php endforeach; ?>
+                <?php endforeach; ?>
+            </div>
         </div>
+    </main>
 
-    </div>
-
+    <!-- FOOTER -->
     <footer>
         <div class="container">
             <div class="row row-cols-4">
@@ -105,6 +113,8 @@ require_once __DIR__ . '/data/db.php';
             </div>
         </div>
     </footer>
+
+    <script src="/js/script.js"></script>
 </body>
 
 </html>
